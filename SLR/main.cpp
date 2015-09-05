e2478e1ae14e4f5c7272a39893a6d669a45f789c
shin_mbpr_late2013 <shocker.0x15@gmail.com>
2015-09-05 22:28:37 +0900

    Initial Commit

---
+++ SLR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SLR
+//
+//  Created by 渡部 心 on 2015/09/05.
+//  Copyright (c) 2015年 渡部 心. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
