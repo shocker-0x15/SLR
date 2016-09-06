8f43b2199612fa96ce145039bdd1fb01d49637b5
shin_mbpr_late2013 <shocker.0x15@gmail.com>
2016-09-07 01:13:44 +0900

    fix the Xcode project file. remove the function to get wavelength selection sample from path samplers.

--- libSLR/Core/light_path_samplers.h
+++ libSLR/Core/light_path_samplers.h
@@ -29,7 +29,6 @@ namespace SLR {
         virtual float getTimeSample(float timeBegin, float timeEnd) = 0;
         virtual PixelPosition getPixelPositionSample(uint32_t baseX, uint32_t baseY) = 0;
         virtual float getWavelengthSample() = 0;
-        virtual float getWLSelectionSample() = 0;
         virtual LensPosSample getLensPosSample() = 0;
         virtual IDFSample getIDFSample() = 0;
         virtual BSDFSample getBSDFSample() = 0;
@@ -50,7 +49,6 @@ namespace SLR {
         float getTimeSample(float timeBegin, float timeEnd) override { float v = m_rng.getFloat0cTo1o(); return timeBegin * (1 - v) + timeEnd * v; }
         PixelPosition getPixelPositionSample(uint32_t baseX, uint32_t baseY) override { return PixelPosition(baseX + m_rng.getFloat0cTo1o(), baseY + m_rng.getFloat0cTo1o()); }
         float getWavelengthSample() override { return m_rng.getFloat0cTo1o(); }
-        float getWLSelectionSample() override { return m_rng.getFloat0cTo1o(); }
         LensPosSample getLensPosSample() override { return LensPosSample(m_rng.getFloat0cTo1o(), m_rng.getFloat0cTo1o()); }
         IDFSample getIDFSample() override { return IDFSample(m_rng.getFloat0cTo1o(), m_rng.getFloat0cTo1o()); }
         BSDFSample getBSDFSample() override { return BSDFSample(m_rng.getFloat0cTo1o(), m_rng.getFloat0cTo1o(), m_rng.getFloat0cTo1o()); }
